9cb30c07a68d3c99b54fa34043bdfe9806f8d757
linhui0705 <linhui0705@qq.com>
2016-03-23 10:38:55 +0800

    修改、新增方法

--- furniture-management-system/src/com/furniture/dao/impl/FurnitureDaoImpl.java
+++ furniture-management-system/src/com/furniture/dao/impl/FurnitureDaoImpl.java
@@ -30,7 +30,7 @@ public class FurnitureDaoImpl {
 	public void UpdateFurniture(Furniture furniture) {
 		try {
 			QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
-			String sql = "update furniture set fname=?,fsummary=?,fnum=?,fprice=? fic=? where fid=?";
+			String sql = "update furniture set fname=?,fsummary=?,fnum=?,fprice=?,fpic=? where fid=?";
 			Object[] params = new Object[] {furniture.getFname(),furniture.getFsummary(),furniture.getFnum(),furniture.getFprice(),furniture.getFpic(),furniture.getFid()};
 			runner.update(sql, params);
 		} catch (Exception e) {
@@ -43,6 +43,28 @@ public class FurnitureDaoImpl {
 			QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
 			String sql = "select * from furniture where fname=?";
 			List<Furniture> furniture1 = runner.query(sql, new BeanListHandler<>(Furniture.class),furniture.getFname());
+			return furniture1;
+		} catch (Exception e) {
+			throw new FurnitureDaoException(e);
+		}
+	}
+	
+	public List<Furniture> getDetail(String fid) {
+		try {
+			QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
+			String sql = "select * from furniture where fid=?";
+			List<Furniture> furniture1 = runner.query(sql, new BeanListHandler<>(Furniture.class),fid);
+			return furniture1;
+		} catch (Exception e) {
+			throw new FurnitureDaoException(e);
+		}
+	}
+	
+	public List<Furniture> getAllFur() {
+		try {
+			QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
+			String sql = "select * from furniture";
+			List<Furniture> furniture1 = runner.query(sql, new BeanListHandler<>(Furniture.class));
 			return furniture1;
 		} catch (Exception e) {
 			throw new FurnitureDaoException(e);
